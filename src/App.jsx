7058452e15a39f0446ee57707a16c56d5fb20d12
khushi <khushibansal@gmail.com>
2024-01-12 23:16:33 -0500

    install react and tailwind css using vite and create first template

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Home from "./pages/home";
 import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 import Profile from "./pages/profile";
+import Header from "./components/header";
+import About from "./pages/About";
 
 export default function App() 
 {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>} Route/>
         <Route path='/signin' element={<SignIn/>} Route/>
         <Route path='/signup' element={<SignUp/>} Route/>
         <Route path='/profile' element={<Profile/>} Route/>
+        <Route path='/about' element={<About/>} Route/>
       </Routes>
     </BrowserRouter>
     
